feb9c5488de5ecaa7f9dd11591b5fea92cb4e81d
winter <jaradm552@gmail.com>
2018-05-19 22:02:21 +1000

    updated scrolls screen and added reading scroll [not finished]
    updated scrolls screen and added reading scroll [not finished]
    
    questions and answer are not implemented properly

---
+++ Clicker Gamification/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    public int gold = 500;
+    int foe = 0;
+    public GameObject[] foes = new GameObject[1];
+    public int[] hp = new int[1];
+	// Use this for initialization
+	void Start ()
+    {
+        InstantiateFoe();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public int Gold()
+    {
+        return gold;
+    }
+    public bool SpendGold(int amount)
+    {
+        if ((gold - amount) >= 0)
+        {
+            gold -= amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    //Activate Foe
+    public void InstantiateFoe()
+    {
+        foes[foe].GetComponent<TapFoe>().Create(hp[foe]);
+        foe++;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance == this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Clicker Gamification/Assets/Scripts/TapFoe.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapFoe : MonoBehaviour
+{
+    int health, maxHealth;
+    public Slider enemyHealth;
+    bool active = false;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(active)
+        {
+            enemyHealth.value = health;
+            if (health <= 0)
+            {
+                //Reward Popup
+
+                //Add Gold
+                GameManager.instance.SpendGold(-10 * maxHealth);
+                //Hide and Deactivate
+                active = false;
+                gameObject.SetActive(false);
+                //Alert GameManager to change
+
+            }
+        }
+	}
+    public void Tap()
+    {
+        health--;
+    }
+    public void Create(int hp)
+    {
+        maxHealth = hp;
+        health = hp;
+        active = true;
+        gameObject.SetActive(true);
+    }
+}
